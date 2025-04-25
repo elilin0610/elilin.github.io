122524ee52bdd492c4ceea40157fd655e34faf70
Eli <s111311536@student.nqu.edu.tw>
2025-04-25 11:44:19 +0800

    hw3

---
+++ homework/03/01.js
@@ -0,0 +1,4 @@
+let num = "42"
+let result = parseInt(num) + 8
+
+console.log(result)
---
+++ homework/03/04.js
@@ -0,0 +1,5 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let result = nums.filter(num => num % 2 === 0)
+
+console.log(result)
